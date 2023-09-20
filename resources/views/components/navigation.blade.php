44271f403ea465004b990ff27e393806392f009b
fajar sidik priatna <jardik.oryza@gmail.com>
2023-09-21 02:50:02 +0700

    add home page and about page

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,4 @@
+<nav class="text-center shadow p-1 text-xl">
+    <a href="{{ route('home') }}" wire:navigate class="mr-4 hover:text-rose-500">Blog</a>
+    <a href="{{ route('projects') }}" wire:navigate class="mr-4 hover:text-rose-500">Projects</a>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Livewire\HomePage::class)->name('home');
+Route::get('projects', App\Livewire\ProjectsPage::class)->name('projects');

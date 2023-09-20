a8e3bb37ddd16588ecdf469754c42d8e703279ed
fajar sidik priatna <jardik.oryza@gmail.com>
2023-09-21 02:49:49 +0700

    add tailwind and quicksand

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+        "./resources/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                quicksand: ['Quicksand'],
+            }
+        }
+    },
+    plugins: []
+}

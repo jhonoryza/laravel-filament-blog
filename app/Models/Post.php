9090b261436433792bc102eb26c326ca13d29a3c
fajar sidik priatna <jardik.oryza@gmail.com>
2023-09-21 01:08:05 +0700

    add post resource

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
@@ -13,5 +14,10 @@ class Post extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'post_categories');
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }

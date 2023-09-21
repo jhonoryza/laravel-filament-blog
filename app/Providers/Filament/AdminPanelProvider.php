1af1b766f1ae2ebb985cce8325c613534c816900
fajar sidik priatna <fajar@suitmedia.com>
2023-09-21 15:21:23 +0700

    set cms font using quicksand

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Filament\FontProviders\SpatieGoogleFontProvider;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -26,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->font('Quicksand', provider: SpatieGoogleFontProvider::class)
             ->login()
             ->sidebarCollapsibleOnDesktop(true)
             ->colors([
